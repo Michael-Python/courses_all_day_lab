d9f9b98e2e05e30be0c6769ee97ce020834cbc00
emilykmcullen <emilycullen135@gmail.com>
2021-01-21 13:41:02 +0000

    mvp routes working

--- src/main/java/com/example/CourseBooking/controllers/CourseController.java
+++ src/main/java/com/example/CourseBooking/controllers/CourseController.java
@@ -1,4 +1,34 @@
 package com.example.CourseBooking.controllers;
 
+import com.example.CourseBooking.repositories.CourseRepository;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping(value="/courses")
+    public ResponseEntity getAllCoursesAndFilters(
+    @RequestParam(required = false, name = "rating") Integer rating,
+    @RequestParam(required = false, name = "customer") String customer
+    ){
+        if(rating != null){
+            return new ResponseEntity(courseRepository.findByRating(rating), HttpStatus.OK);
+        }
+        if(customer != null){
+            return new ResponseEntity(courseRepository.findByBookingsCustomerName(customer), HttpStatus.OK);
+        }
+        return new ResponseEntity(courseRepository.findAll(), HttpStatus.OK);
+    }
+
+
+
 }
--- src/main/java/com/example/CourseBooking/controllers/CustomerController.java
+++ src/main/java/com/example/CourseBooking/controllers/CustomerController.java
@@ -1,4 +1,26 @@
 package com.example.CourseBooking.controllers;
 
+import com.example.CourseBooking.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity getAllCustomersAndFilters(
+            @RequestParam(required = false, name = "course") String course
+    ){
+        if(course != null){
+            return new ResponseEntity(customerRepository.findByBookingsCourseName(course), HttpStatus.OK);
+        }
+        return new ResponseEntity(customerRepository.findAll(), HttpStatus.OK);
+    }
 }
